f2d643da14f384cd5d93d1dccc0b85d38a8957f0
Seva Safris <seva@safris.org>
2016-02-08 12:49:38 -0800

    Upgrade commons.dbcp to Apache dbcp2.

--- src/main/java/org/safris/commons/dbcp/DataSources.java
+++ src/main/java/org/safris/commons/dbcp/DataSources.java
@@ -3,11 +3,16 @@ package org.safris.commons.dbcp;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.safris.commons.logging.LoggerPrintWriter;
 import org.safris.xml.generator.compiler.runtime.BindingRuntimeException;
 
 public final class DataSources {
+  private static final Logger logger = Logger.getLogger(DataSources.class.getName());
+
   public static BasicDataSource createDataSource(final List<$dbcp_dbcp> dbcps, final String name) throws SQLException {
     for (final $dbcp_dbcp dbcp : dbcps)
       if (name.equals(dbcp._name$().text()))
@@ -22,7 +27,7 @@ public final class DataSources {
 
     final dbcp_dbcp._jdbc jdbc = dbcp._jdbc(0);
     final BasicDataSource dataSource = new BasicDataSource();
-    if (jdbc._driverClassName() == null || jdbc._driverClassName().size() == 0 || jdbc._driverClassName(0).text() == null)
+    if (!jdbc._driverClassName(0).isNull())
       throw new BindingRuntimeException("/dbcp:jdbc/dbcp:driverClassName is missing");
 
     dataSource.setDriverClassName(jdbc._driverClassName(0).text());
@@ -37,34 +42,33 @@ public final class DataSources {
 //      }
 //  }
 
-    if (jdbc._url() == null || jdbc._url().size() == 0 || jdbc._url(0).text() == null)
+    if (!jdbc._url(0).isNull())
       throw new BindingRuntimeException("/dbcp:jdbc/dbcp:url is missing");
 
     dataSource.setUrl(jdbc._url(0).text());
 
-    if (jdbc._username() == null || jdbc._username().size() == 0 || jdbc._username(0).text() == null)
+    if (!jdbc._username(0).isNull())
       throw new BindingRuntimeException("/dbcp:jdbc/dbcp:username is missing");
 
     dataSource.setUsername(jdbc._username(0).text());
 
-    if (jdbc._password() == null || jdbc._password().size() == 0 || jdbc._password(0).text() == null)
+    if (!jdbc._password(0).isNull())
       throw new BindingRuntimeException("/dbcp:jdbc/dbcp:password is missing");
 
     dataSource.setPassword(jdbc._password(0).text());
-
     final dbcp_dbcp._default defaults = dbcp._default(0);
-    if (defaults._connectionProperties() != null && defaults._connectionProperties().size() != 0 && defaults._connectionProperties(0)._property() != null && defaults._connectionProperties(0)._property().size() != 0)
+    if (!defaults._connectionProperties(0).isNull())
       for (final dbcp_dbcp._default._connectionProperties._property property : defaults._connectionProperties(0)._property())
         if (property._name$() != null && property._name$().text() != null && property._value$() != null && property._value$().text() != null)
           dataSource.addConnectionProperty(property._name$().text(), property._value$().text());
 
-    if (defaults._autoCommit() != null && defaults._autoCommit().size() != 0 && defaults._autoCommit(0).text() != null)
+    if (!defaults._autoCommit(0).isNull())
       dataSource.setDefaultAutoCommit(defaults._autoCommit(0).text());
 
-    if (defaults._readOnly() != null && defaults._readOnly().size() != 0 && defaults._readOnly(0).text() != null)
+    if (!defaults._readOnly(0).isNull())
       dataSource.setDefaultReadOnly(defaults._readOnly(0).text());
 
-    if (defaults._transactionIsolation() != null && defaults._transactionIsolation().size() != 0 && defaults._transactionIsolation(0).text() != null) {
+    if (!defaults._transactionIsolation(0).isNull()) {
       if (dbcp_dbcp._default._transactionIsolation.NONE.text().equals(defaults._transactionIsolation(0).text()))
         dataSource.setDefaultTransactionIsolation(Connection.TRANSACTION_NONE);
       else if (dbcp_dbcp._default._transactionIsolation.READ_5FCOMMITTED.text().equals(defaults._transactionIsolation(0).text()))
@@ -75,16 +79,17 @@ public final class DataSources {
         dataSource.setDefaultTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ);
       else if (dbcp_dbcp._default._transactionIsolation.SERIALIZABLE.text().equals(defaults._transactionIsolation(0).text()))
         dataSource.setDefaultTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+      else
+        throw new UnsupportedOperationException("Unsupported transaction isolation: " + defaults._transactionIsolation(0).text());
     }
 
-    if (dbcp._size() != null && dbcp._size().size() != 0) {
-      final dbcp_dbcp._size size = dbcp._size(0);
-
+    final dbcp_dbcp._size size = dbcp._size(0);
+    if (!size.isNull()) {
       if (!size._initialSize(0).isNull())
         dataSource.setInitialSize(size._initialSize(0).text().intValue());
 
       if (!size._maxActive(0).isNull())
-        dataSource.setMaxActive(size._maxActive(0).text().intValue());
+        dataSource.setMaxTotal(size._maxActive(0).text().intValue());
 
       if (!size._maxIdle(0).isNull())
         dataSource.setMaxIdle(size._maxIdle(0).text().intValue());
@@ -93,12 +98,11 @@ public final class DataSources {
         dataSource.setMinIdle(size._minIdle(0).text().intValue());
 
       if (!size._maxWait(0).isNull())
-        dataSource.setMaxWait(size._maxWait(0).text().intValue());
+        dataSource.setMaxWaitMillis(size._maxWait(0).text().intValue());
     }
 
-    if (dbcp._management() != null && dbcp._management().size() != 0) {
-      final dbcp_dbcp._management management = dbcp._management(0);
-
+    final dbcp_dbcp._management management = dbcp._management(0);
+    if (!management.isNull()) {
       if (!management._validationQuery(0).isNull())
         dataSource.setValidationQuery(management._validationQuery(0).text());
 
@@ -121,9 +125,8 @@ public final class DataSources {
         dataSource.setMinEvictableIdleTimeMillis(management._minEvictableIdleTime(0).text().intValue());
     }
 
-    if (dbcp._preparedStatements() != null && dbcp._preparedStatements().size() != 0) {
-      final dbcp_dbcp._preparedStatements preparedStatements = dbcp._preparedStatements(0);
-
+    final dbcp_dbcp._preparedStatements preparedStatements = dbcp._preparedStatements(0);
+    if (!preparedStatements.isNull()) {
       if (!preparedStatements._poolPreparedStatements(0).isNull())
         dataSource.setPoolPreparedStatements(preparedStatements._poolPreparedStatements(0).text());
 
@@ -131,17 +134,27 @@ public final class DataSources {
         dataSource.setMaxOpenPreparedStatements(preparedStatements._maxOpenPreparedStatements(0).text().intValue());
     }
 
-    if (dbcp._removal() != null && dbcp._removal().size() != 0) {
-      final dbcp_dbcp._removal removal = dbcp._removal(0);
-
+    final dbcp_dbcp._removal removal = dbcp._removal(0);
+    if (!removal.isNull()) {
       if (!removal._removeAbandoned(0).isNull())
-        dataSource.setRemoveAbandoned(removal._removeAbandoned(0).text());
+        dataSource.setRemoveAbandonedOnBorrow(removal._removeAbandoned(0).text());
 
       if (!removal._removeAbandonedTimeout(0).isNull())
         dataSource.setRemoveAbandonedTimeout(removal._removeAbandonedTimeout(0).text().intValue());
 
       if (!removal._logAbandoned(0).isNull())
         dataSource.setLogAbandoned(removal._logAbandoned(0).text());
+    }
+
+    final dbcp_dbcp._logging logging = dbcp._logging(0);
+    if (!logging.isNull()) {
+      final LoggerPrintWriter loggerPrintWriter = new LoggerPrintWriter(logger, Level.parse(logging._level(0).text()));
+      dataSource.setLogWriter(loggerPrintWriter);
+      dataSource.setLogExpiredConnections(!logging._logExpiredConnections(0).isNull() && logging._logExpiredConnections(0).text());
+      if (!logging._logAbandoned(0).isNull() && logging._logAbandoned(0).text()) {
+        dataSource.setAbandonedLogWriter(loggerPrintWriter);
+        dataSource.setLogAbandoned(true);
+      }
     }
 
     return dataSource;
