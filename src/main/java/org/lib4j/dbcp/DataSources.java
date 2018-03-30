3bb3d6c5c04852b2c939e45153f38f0e3b3137eb
Seva Safris <seva@safris.org>
2018-03-30 22:27:50 +0700

    Begin next SNAPSHOT version

--- src/main/java/org/lib4j/dbcp/DataSources.java
+++ src/main/java/org/lib4j/dbcp/DataSources.java
@@ -37,7 +37,7 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.lib4j.dbcp_2_0_3.Dbcp;
+import org.lib4j.dbcp_2_0_4.Dbcp;
 import org.lib4j.logging.LoggerPrintWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
